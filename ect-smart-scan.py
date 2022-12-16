d7328fef27e0165f528e3827b76b61e6d38f5560
Matt Chandler <mc16535@bristol.ac.uk>
2022-12-16 16:27:27 +0000

    Two working examples for use of a handyscope present. New Handyscope class preferred as it can handle all the data collection itself, and can probably deal with the cleanup better.

--- ect-smart-scan.py
+++ ect-smart-scan.py
@@ -35,51 +35,61 @@ if __name__ == "__main__":
         Library.enable_device_db_store()
     except NotImplementedError:
         pass #TODO: Demand local storage of device database
-        
-    T = 10
-    r = 25
-    N = 32
     
     gen_freq = 10e3
     n_cycles = 5
-    n_samples_per_cycle = 10
+    n_samples_per_cycle = 100
     sample_freq = int(gen_freq * n_samples_per_cycle)
-    record_length = int(n_cycles * sample_freq / gen_freq)
+    record_length = int(n_cycles * sample_freq / gen_freq) + 1
     
     # ltp.network.auto_detect_enabled = True
     ltp.device_list.update()
     
+    handyscope = hs.Handyscope(gen_freq, 0.1, sample_freq, record_length, 4)
+    np_data = np.asarray(handyscope.get_record())
+    t = np.linspace(0, n_cycles / gen_freq, record_length)
+    plt.plot(t * 1e6, np_data[0, :])
+    plt.xlabel("Time (us)")
+    plt.ylabel("Voltage (V)")
+    plt.show()
+    del handyscope
+    
     gen_idx = h.find_gen(ltp.device_list)
     scp_idx = h.find_scp(ltp.device_list)
+    
     with hs.open_gen(ltp.device_list.get_item_by_index(gen_idx), gen_freq, 0.1) as gen:
-        print(gen.name)
-        print(gen.type)
         with hs.open_scp(ltp.device_list.get_item_by_index(scp_idx), sample_freq, record_length, 4) as scp:
             # Do all the handyscope stuff here
-            print(scp.name)
-            print(scp.type)
+            scp.start()
+            gen.start()
             
-            # gen.signal_type = ltp.ST_SINE
-            # gen.frequency = gen_freq
-            # gen.amplitude = 0.1
-            # gen.offset = 0
-            # gen.output_on = True
+            while not scp.is_data_ready:
+                time.sleep(.01)
             
-            # scp.measure_mode = ltp.MM_BLOCK
-            # scp.sample_frequency = sample_freq
-            # scp.record_length = record_length
-            # # scp.pre_sample_ratio = 0
-            # for ch in scp.channels:
-            #     ch.enabled = True
-            #     ch.range = 4
-            #     ch.coupling = ltp.CK_ACV
-            # scp.trigger_time_out = 1
-            # for ch in scp.channels:
-            #     ch.trigger.enabled = False
-            # ch = scp.channels[0]
-            # # ch.trigger.kind = ltp.TK_RISINGEDGE
-            # # ch.trigger.hystereses[0]
+            data = scp.get_data()
             
+            gen.stop()
+            
+            np_data = np.asarray(data)
+            t = np.linspace(0, n_cycles / gen_freq, record_length)
+            plt.plot(t * 1e6, np_data[0, :])
+            plt.xlabel("Time (us)")
+            plt.ylabel("Voltage (V)")
+            plt.show()
+    
+    gen, scp = None, None
+    
+    ltp.device_list.update()
+    
+    gen_freq = 20e3
+    n_cycles = 10
+    n_samples_per_cycle = 50
+    sample_freq = int(gen_freq * n_samples_per_cycle)
+    record_length = int(n_cycles * sample_freq / gen_freq) + 1
+    
+    with hs.open_gen(ltp.device_list.get_item_by_index(gen_idx), gen_freq, 0.1) as gen:
+        with hs.open_scp(ltp.device_list.get_item_by_index(scp_idx), sample_freq, record_length, 4) as scp:
+            # Do all the handyscope stuff here
             scp.start()
             gen.start()
             
--- handyscope.py
+++ handyscope.py
@@ -7,8 +7,12 @@ Created on Tue Dec 13 16:36:22 2022
 A file containing classes which wrap around the generator and oscilloscope
 classes from libtiepie, to enable compatibility with "with" statements.
 """
+from helpers import find_gen, find_scp
 import libtiepie as ltp
+import libtiepie.api as api
 from libtiepie.devicelistitem import DeviceListItem
+import time
+import warnings
 
 class open_gen:
     def __init__(self, generator_item, frequency, v_amp, signal_type=ltp.ST_SINE, dc_offset=0):
@@ -17,19 +21,20 @@ class open_gen:
         self.v_amp       = v_amp
         self.signal_type = signal_type
         self.dc_offset   = dc_offset
+        self.gen = None
     
     def __enter__(self):
-        gen = self.generator.open_generator()
-        gen.signal_type = self.signal_type
-        gen.frequency   = self.frequency
-        gen.amplitude   = self.v_amp
-        gen.offset      = self.dc_offset
-        gen.output_on   = True
-        return gen
+        self.gen = self.generator.open_generator()
+        self.gen.signal_type = self.signal_type
+        self.gen.frequency   = self.frequency
+        self.gen.amplitude   = self.v_amp
+        self.gen.offset      = self.dc_offset
+        self.gen.output_on   = True
+        return self.gen
     
     def __exit__(self, exc_type, exc_value, traceback):
-        # print(**kwargs)
-        self.generator = None
+        warnings.warn("Generator is not deleted manually - to reuse the same device later in the program, delete the variable.")
+        self.gen = None
 
 class open_scp:
     def __init__(self, oscilloscope_item, frequency, record_length, v_range, measure_mode=ltp.MM_BLOCK, active_channels=-1, channel_coupling=ltp.CK_ACV):
@@ -40,13 +45,14 @@ class open_scp:
         self.measure_mode     = measure_mode
         self.active_channels  = active_channels
         self.channel_coupling = channel_coupling
+        self.scp = None
         
     def __enter__(self):
-        scp                  = self.oscilloscope.open_oscilloscope()
-        scp.measure_mode     = self.measure_mode
-        scp.sample_frequency = self.frequency
-        scp.record_length    = self.record_length
-        for idx, ch in enumerate(scp.channels):
+        self.scp                  = self.oscilloscope.open_oscilloscope()
+        self.scp.measure_mode     = self.measure_mode
+        self.scp.sample_frequency = self.frequency
+        self.scp.record_length    = self.record_length
+        for idx, ch in enumerate(self.scp.channels):
             if self.active_channels == -1 or idx in self.active_channels:
                 ch.enabled  = True
                 ch.range    = self.v_range
@@ -57,8 +63,60 @@ class open_scp:
         # for ch in scp.channels:
         #     ch.trigger.enabled = False
         # ch = scp.channels[0]
-        return scp
+        return self.scp
     
     def __exit__(self, exc_type, exc_value, traceback):
-        # print(**kwargs)
-        self.oscilloscope = None
+        warnings.warn("Oscilloscope is not deleted manually - to reuse the same device later in the program, delete the variable.")
+        self.scp = None
+        
+class Handyscope:
+    __slots__ = ('gen', 'scp')
+    
+    def __init__(self, input_frequency, input_amplitude, output_sample_frequency, output_record_length, output_range, input_signal_type=ltp.ST_SINE, input_offset=0, output_measure_mode=ltp.MM_BLOCK, output_active_channels=-1, output_channel_coupling=ltp.CK_ACV):
+        # self._input_frequency         = input_frequency
+        # self._input_amplitude         = input_amplitude
+        # self._input_signal_type       = input_signal_type
+        # self._input_offset            = input_offset
+        # self._output_sample_frequency = output_sample_frequency
+        # self._output_record_length    = output_record_length
+        # self._output_range            = output_range
+        # self._output_measure_mode     = output_measure_mode
+        # self._output_active_channels  = output_active_channels
+        # self._output_channel_coupling = output_channel_coupling
+        
+        # gen_idx = h.find_gen(ltp.device_list)
+        # scp_idx = h.find_scp(ltp.device_list)
+        
+        self.gen = ltp.device_list.get_item_by_index(find_gen(ltp.device_list)).open_generator()
+        self.scp = ltp.device_list.get_item_by_index(find_scp(ltp.device_list)).open_oscilloscope()
+        
+        self.gen.signal_type = input_signal_type
+        self.gen.frequency   = input_frequency
+        self.gen.amplitude   = input_amplitude
+        self.gen.offset      = input_offset
+        self.gen.output_on   = True
+        
+        self.scp.measure_mode     = output_measure_mode
+        self.scp.sample_frequency = output_sample_frequency
+        self.scp.record_length    = output_record_length
+        for idx, ch in enumerate(self.scp.channels):
+            if output_active_channels == -1 or idx in output_active_channels:
+                ch.enabled  = True
+                ch.range    = output_range
+                ch.coupling = output_channel_coupling
+            else:
+                ch.enabled  = False
+    
+    def get_record(self):
+        self.scp.start()
+        self.gen.start()
+        
+        while not self.scp.is_data_ready:
+            time.sleep(.01)
+        
+        data = self.scp.get_data()
+        
+        self.gen.stop()
+        # self.scp.stop()
+        
+        return data
